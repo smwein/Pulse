e8a97cf953b6868991d29fbf6c45413a5260e2f7
smwein <smwein@gmail.com>
2026-04-27 12:58:13 -0500

    chore(worker): remove passWithNoTests now that suite has tests

--- worker/vitest.config.ts
+++ worker/vitest.config.ts
@@ -2,7 +2,6 @@ import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config";
 
 export default defineWorkersConfig({
   test: {
-    passWithNoTests: true,
     poolOptions: {
       workers: {
         wrangler: { configPath: "./wrangler.toml" },
